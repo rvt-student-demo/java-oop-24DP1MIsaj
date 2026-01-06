fc3c298e8baa13422b230cb65a34be4853ad5c3a
Marats Isajevs <A242277MI>
2026-01-06 21:05:46 +0200

    Kommit Orders

--- src/main/java/rvt/Orders.java
+++ src/main/java/rvt/Orders.java
@@ -1,22 +1,32 @@
 package rvt;
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.Scanner;
 
 public class Orders {
-    try (Scanner reader = new Scanner(new File("rvt/orders.csv"))) {
-        reader.nextLine();
-        while (reader.hasNextLine()) {
-            String row = reader.nextLine();
-            String[] parts = row.split(",");
+    public static void main(String[] args) {
+         try (Scanner reader = new Scanner(new File("data/orders.csv"))) {
+            reader.nextLine();
 
-            String OrderID = parts[0];
-            String Customer = parts[1];
-            String Product = parts[2];
-            int Quantity = Integer.valueOf(parts[3]);
-            float Price = Integer.valueOf(parts[4]);
+            double result = 0;
+            while (reader.hasNextLine()) {
+                String row = reader.nextLine();
+                String[] parts = row.split(",");
 
-            System.out.println("Pasūtījums #" + OrderID + ": " + Customer + " pasūtīja " + Quantity + " x " + Product + " (" + Price + " EUR) -> Kopā: " + (Quantity * Price) + " EUR");
+                String OrderID = parts[0];
+                String Customer = parts[1];
+                String Product = parts[2];
+                int Quantity = Integer.valueOf(parts[3]);
+                double Price = Double.valueOf(parts[4]);
 
+                System.out.println("Pasūtījums #" + OrderID + ": " + Customer + " pasūtīja " + Quantity + " x " + Product + " (" + Price + " EUR) -> Kopā: " + (Quantity * Price) + " EUR");
+                result += Quantity * Price;
+            }
+
+            System.out.println("Kopējā pasūtījumu summa: " + result);
+            
+        } catch (FileNotFoundException e) {
+            System.out.println("Fails orders.csv netika atrasts!");
         }
     }
 }
