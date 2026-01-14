a6dacf9156286cfcfd382bdec44328416789a3d1
24DP1MIsaj <A242277MI@rvt.lv>
2026-01-14 16:05:45 +0200

    Kommit To do list (2 parts)

---
+++ src/main/java/rvt/TodoList.java
@@ -0,0 +1,41 @@
+package rvt;
+import java.util.ArrayList;
+
+public class TodoList {
+    private ArrayList<String> tasks;
+
+    public TodoList() {
+        this.tasks = new ArrayList<>();
+    }
+    
+    public void add(String task) {
+        tasks.add(task);
+    }
+
+    public void print() {
+        for (int i = 0; i < tasks.size(); i++) {
+            System.out.println((i + 1) + ": " + tasks.get(i));
+        }
+    }
+
+    public void remove(int number) {
+        if (number >= 1 && number <= tasks.size()) {
+            tasks.remove(number - 1);
+        }
+    }
+
+    public static void main(String[] args) {
+        TodoList list = new TodoList();
+        list.add("read the course material");
+        list.add("watch the latest fool us");
+        list.add("take it easy");
+        list.print();
+        list.remove(2);
+        list.print();
+        list.add("buy raisins");
+        list.print();
+        list.remove(1);
+        list.remove(1);
+        list.print();
+    }
+}
