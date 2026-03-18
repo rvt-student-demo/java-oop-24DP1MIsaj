1fd95b6f8cb22fbbcf9c992e7815de165db45582
Marats Isajevs <A242277MI>
2026-03-18 16:55:33 +0200

    Commit Online shop (8 parts)

--- src/main/java/rvt/shop/ShoppingCart.java
+++ src/main/java/rvt/shop/ShoppingCart.java
@@ -1,20 +1,36 @@
 package rvt.shop;
 import java.util.HashMap;
+import java.util.Map;
 
 public class ShoppingCart {
-    private String product;
-    private int price;
 
-    public ShoppingCart(String product, int price) {
-        this.product = product;
-        this.price = price;
+    private Map<String, Item> items;
+
+    public ShoppingCart() {
+        this.items = new HashMap<>();
     }
 
     public void add(String product, int price) {
-
+        if (items.containsKey(product)) {
+            items.get(product).increaseQuantity();
+        } else {
+            items.put(product, new Item(product, 1, price));
+        }
     }
 
     public int price() {
+        int total = 0;
 
+        for (Item item : items.values()) {
+            total += item.price();
+        }
+
+        return total;
+    }
+
+    public void print() {
+        for (Item item : items.values()) {
+            System.out.println(item);
+        }
     }
 }
--- src/main/java/rvt/shop/Store.java
+++ src/main/java/rvt/shop/Store.java
@@ -11,9 +11,9 @@ public class Store {
         this.scanner = scanner;
     }
 
-    // the method that handles the customers visit to the store.
     public void shop(String customer) {
         ShoppingCart cart = new ShoppingCart();
+
         System.out.println("Welcome to the store " + customer);
         System.out.println("our selection:");
 
@@ -24,13 +24,16 @@ public class Store {
         while (true) {
             System.out.print("What to put in the cart (press enter to go to the register): ");
             String product = scanner.nextLine();
+
             if (product.isEmpty()) {
                 break;
             }
 
-            // Add code here that adds the product to the cart,
-            // If there is any in the warehouse, and reduces the stock in the warehouse
-            // Don't touch any of the other code!
+            if (warehouse.take(product)) {
+                cart.add(product, warehouse.price(product));
+            } else {
+                System.out.println("Product not available.");
+            }
         }
 
         System.out.println("your shoppingcart contents:");
