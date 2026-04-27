4b450e94971251ce794117e7a8935733be9be4da
24DP1MIsaj <A242277MI@rvt.lv>
2026-04-27 09:58:10 +0300

    Commit Show and Ticket

---
+++ src/main/java/rvt/ShowAndTicket/Show.java
@@ -0,0 +1,11 @@
+package rvt.ShowAndTicket;
+
+public class Show {
+    private String movie;
+    private String time;
+
+    public Show(String movie, String time) {
+        this.movie = movie;
+        this.time = time;
+    }
+}
---
+++ src/main/java/rvt/ShowAndTicket/Ticket.java
@@ -0,0 +1,13 @@
+package rvt.ShowAndTicket;
+
+public class Ticket {
+    private int seat;
+    private int code;
+    private Show show;
+
+    public Ticket(int seat, int code, Show show) {
+        this.seat = seat;
+        this.code = code;
+        this.show = show;
+    }
+}
